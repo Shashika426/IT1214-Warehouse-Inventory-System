c9892d46c3718ad5a2868e84c7f9930d95917332
Shashika426 <sankaegodagedara@gmail.com>
2026-04-24 20:55:14 +0530

    Completed warehouse system

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # IT1214-Warehouse-Inventory-System
+
+## Project Description
+this is aa simple Warehouse Inventory  Management System developed using Java. It allows users to manage inventory items such as
+adding, removing, updating, searching and displaying items.
+
+## Features
+-Add new item
+-Remove item
+-Update item quantity
+-Search item by ID or name
+-Display all items
+
+## Tehnologies used
+-Java
+-Java Collections Framework (ArrayList)
+
+## How to Run
+1.Compile the files;
+  javac *.javac
+  
+2.Run the program:
+  java Warehouse
+  
+## Sample Output
+---Warehouse Menu---
+  1.Add Item
+  2.Remove Item
+  3.Update Item
+  4.Search Item
+  5.Display Item
+  6.Exit
+  
+Enter choice: 1
+Enter ID: 001
+Enter Name: Pen
+Enter Quantity: 10
+Enter Price: 50
+
+Item added successfully!
---
+++ Warehouse.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Warehouse{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		Inventory inventory=new Inventory();
+		
+		while (true){
+			System.out.println("\n---Warehouse Menu---");
+			System.out.println("1. Add Item");
+			System.out.println("2. Remove Item");
+			System.out.println("3. Update Item");
+			System.out.println("4. Search Item");
+			System.out.println("5. Display Items");
+			System.out.println("6. Exit");
+			System.out.println("Enter choice: ");
+			int choice=sc.nextInt();
+			sc.nextLine();
+			
+			switch(choice){
+				case 1:
+					System.out.print("Enter ID: ");
+					String id=sc.nextLine();
+					System.out.print("Enter Name: ");
+					String name=sc.nextLine();
+					System.out.print("Enter Quantity: ");
+					int qty=sc.nextInt();
+					System.out.print("Enter Price: ");
+					double price=sc.nextDouble();
+					inventory.addItem(new Item(id,name,qty,price));
+					break;
+					
+				case 2:
+					System.out.print("Enter ID to Remove: ");
+					inventory.removeItem(sc.nextLine());
+					break;
+					
+				case 3:
+					System.out.print("Enter ID to Update: ");
+					String upId=sc.nextLine();
+					System.out.print("Enter New Quantity: ");
+					int newQty=sc.nextInt();
+					inventory.updateItem(upId,newQty);
+					break;
+				
+				case 4:
+					System.out.print("Enter ID or Name: ");
+					inventory.searchItem(sc.nextLine());
+					break;
+				
+				case 5:
+					inventory.displayItems();
+					break;
+				
+				case 6:
+					System.out.println("Exiting.......");
+					return;
+					
+				default:
+					System.out.println("Invalid choice!");
+			}
+		}
+	}
+}
